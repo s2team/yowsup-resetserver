ae71db277a00cb37fee91ac9533ecc1b1ded30e7
s2team <git@s2team.codes>
2015-01-11 18:09:08 +0100

    Added spring security

--- src/main/java/de/votesapp/yowsuprest/YowsuprestApplication.java
+++ src/main/java/de/votesapp/yowsuprest/YowsuprestApplication.java
@@ -2,14 +2,12 @@ package de.votesapp.yowsuprest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableConfigurationProperties
 public class YowsuprestApplication {
 
 	public static void main(final String[] args) {
